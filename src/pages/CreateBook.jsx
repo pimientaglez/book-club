b531298efa8462719bbe8a0a03c9e54af4dba16f
Ricardo Pimienta <pimientaglez@gmail.com>
2024-02-27 11:49:25 -0600

    Create book flow

--- src/pages/CreateBook.jsx
+++ src/pages/CreateBook.jsx
@@ -1,7 +1,125 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const CreateBook = () => {
-  return (
-    <div>CreateBook</div>
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [cover, setCover] = useState("");
+    const [intro, setIntro] = useState("");
+    const [completed, setCompleted] = useState(false);
+    const [review, setReview] = useState("");
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        switch (name) {
+            case "title": 
+                setTitle(value);
+                break;
+            case "author": 
+                setAuthor(value);
+                break;
+            case "intro": 
+                setIntro(value);
+                break;
+            case "completed": 
+                setCompleted(e.target.checked);
+                break;
+            case "review": 
+                setReview(value);
+                break;
+            default:
+        }
+    }
+    const handleChangeFile = (e) => {
+        const element = e.target;
+        const file = element.files[0];
+        const reader = new FileReader();
+        
+        reader.readAsDataURL(file);
+
+        reader.onloadend = () => {
+            setCover(reader.result.toString());
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newBook = {
+            id: crypto.randomUUID(),
+            title,
+            author,
+            cover,
+            intro,
+            completed,
+            review,
+        }
+
+        //TODO: create book
+    }
+
+    return (
+    <>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <div>Title</div>
+                <input 
+                    type="text" 
+                    name="title" 
+                    onChange={handleChange} 
+                    value={title}
+                />
+            </div>
+            <div>
+                <div>Author</div>
+                <input 
+                    type="text" 
+                    name="author" 
+                    onChange={handleChange} 
+                    value={author}
+                />
+            </div>
+            <div>
+                <div>Cover</div>
+                <input 
+                    type="file" 
+                    name="cover" 
+                    onChange={handleChangeFile} 
+                />
+                <div>{!!cover ? <img src={cover} width="200px" alt="preview"/> : ""}</div>
+            </div>
+            <div>
+                <div>Intro</div>
+                <input 
+                    type="text" 
+                    name="intro" 
+                    onChange={handleChange} 
+                    value={intro}
+                />
+            </div>
+            <div>
+                <div>Completed</div>
+                <input 
+                    type="checkbox" 
+                    name="completed" 
+                    onChange={handleChange} 
+                    value={completed}
+                />
+            </div>
+            <div>
+                <div>Review</div>
+                <input 
+                    type="text" 
+                    name="review" 
+                    onChange={handleChange} 
+                    value={review}
+                />
+            </div>
+            <div>
+                <input type="submit" value="Register Book" />
+            </div>
+        </form>
+    </>
   )
 }
