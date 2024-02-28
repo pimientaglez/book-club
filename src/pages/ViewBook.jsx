f25d6de3ad7ace5e3da393821c3120f4769a66c1
Ricardo Pimienta <pimientaglez@gmail.com>
2024-02-28 14:48:05 -0600

    ViewBook  styling

--- src/pages/ViewBook.jsx
+++ src/pages/ViewBook.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { Layout } from '../components/Layout'
 import { useAppContext } from '../store/Store';
 import { useParams } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import { FcCheckmark } from "react-icons/fc";
+import { TbProgress } from "react-icons/tb";
+
 
 export const ViewBook = () => {
     const [book, setBook] = useState({});
@@ -16,13 +20,20 @@ export const ViewBook = () => {
     }
     return (
     <Layout>
-        View
-        <h2>{book?.title}</h2>
-        <div>{book?.cover? <img src={book.cover} width="400" alt={book.title}/> : '' }</div>
-        <div>{book?.author}</div>
-        <div>{book?.intro}</div>
-        <div>{book?.completed ? 'Leido': 'Por terminar'}</div>
-        <div>{book?.review}</div>
+        <Container style={{width:'50%', marginTop:'3em', marginBottom:'3em', display: 'flex'}}>
+            <div>
+                {book?.cover? <img src={book.cover} width="400" alt={book.title}/> : '' }
+            </div>
+            <div className="ms-4">
+                <h2>{book?.title}</h2>
+                <div><span className="fs-5">Author: </span><span>{book?.author}</span></div>
+                <div>{book?.intro}</div>
+                <div>{book?.completed ? <div><FcCheckmark/> Completed</div>: <div><TbProgress/> Completed</div>}</div>
+                <div><span className="fs-5">Review: </span><span>{book?.review}</span></div>
+            </div>
+
+        </Container>
+        
     </Layout>
   )
 }
