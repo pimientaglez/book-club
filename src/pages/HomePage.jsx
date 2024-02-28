bdbcdd05188e7b63fb473a83fc5322af588e8286
Ricardo Pimienta <pimientaglez@gmail.com>
2024-02-28 12:08:41 -0600

    Implemented grid layout for book cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { useAppContext } from '../store/Store'
 import { Layout } from '../components/Layout';
 import { Book } from '../components/Book';
+import Container from 'react-bootstrap/Container';
 
 export const HomePage = () => {
     const {items} = useAppContext();
   return (
     <Layout>
-        {items.map(item => <Book key={item.id} item={item}/> )}
+        <Container style={{width:'70%', marginTop:'3em', marginBottom:'3em', display:'grid', gridTemplateColumns:'repeat(4,1fr)', gridColumnGap: '2em', gridRowGap: '2em' }}>
+            {items.map(item => <Book key={item.id} item={item}/> )}
+        </Container>
     </Layout>
   )
 }
