86f437a1900ba8d129746cf388d1e9399661c097
Ricardo Pimienta <pimientaglez@gmail.com>
2024-02-28 12:08:22 -0600

    implemented Book Card style

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export const Book = ({item}) => {
   return (
-    <div>
-        <Link to={`/view/${item.id}`}>
-            <img src={item.cover} width="200" alt={item.title}/>
-            <h2>{item.title}</h2>
-        </Link>
-    </div>
+    <Card style={{ width: '18rem' }}>
+        <Card.Img 
+            variant="top"
+            src={item.cover} 
+            alt={item.title}
+        />
+        <Card.Body>
+            <Card.Title>{item.title}</Card.Title>
+            <Card.Text>
+                {item.intro}
+            </Card.Text>
+            <Button variant="primary"><Link to={`/view/${item.id}` } className="nav-link">See book</Link></Button>
+        </Card.Body>
+    </Card>
   )
 }
