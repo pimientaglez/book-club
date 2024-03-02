904897cc68b15c2ac1add4a86c93d538f925cda7
Ricardo Pimienta <pimientaglez@gmail.com>
2024-03-01 20:27:58 -0600

    format card book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import coverNotFound from '../img/cover-not-found.png';
+import { truncateText } from '../helpers/helper';
 
 export const Book = ({item}) => {
   return (
@@ -13,7 +14,7 @@ export const Book = ({item}) => {
                 variant="top"
                 src={item.cover} 
                 alt={item.title}
-                className="cover-book"
+                className="card-cover"
             />
           ) : (
             <Card.Img 
@@ -24,9 +25,11 @@ export const Book = ({item}) => {
           )
         }
         <Card.Body>
-            <Card.Title>{item.title}</Card.Title>
-            <Card.Text>
-                {item.intro? (item.intro.length > 100 ? `${item.intro.substring(0, 99)}...`: item.intro) : ''}
+            <Card.Title className="card-title">
+              {truncateText(item.title, 50)}
+            </Card.Title>
+            <Card.Text className="card-description">
+                {truncateText(item.intro, 100)}
             </Card.Text>
             <Button variant="primary"><Link to={`/view/${item.id}` } className="nav-link">See book</Link></Button>
         </Card.Body>
--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -4,8 +4,16 @@ export const convertBook = (googleBook) => {
         title: googleBook.volumeInfo.title,
         author: googleBook.volumeInfo.authors? googleBook.volumeInfo.authors[0]: '',
         cover: googleBook.volumeInfo.imageLinks?  googleBook.volumeInfo.imageLinks.thumbnail : '',
-        intro: googleBook.volumeInfo.description,
+        intro: googleBook.volumeInfo.description ? googleBook.volumeInfo.description : 'No description available',
         completed: false,
         review: '',
     } 
+}
+export const truncateText = (text, maxLength) => {
+    if (!text) return '';
+    if (text.length > maxLength) {
+        return `${text.substring(0, maxLength - 1)}...`
+    } else {
+        return text
+    }
 }
