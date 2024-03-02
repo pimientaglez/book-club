cae7b05e5ec3adef8d3a25a58e008377ffeee2cf
Ricardo Pimienta <pimientaglez@gmail.com>
2024-03-01 23:07:47 -0600

    create firebase file with configuration

---
+++ src/firebaseConfig/firebase.jsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0Ch55Ec961xeSeUsutYd6VWWam4RijoU",
+  authDomain: "book-app-654a8.firebaseapp.com",
+  projectId: "book-app-654a8",
+  storageBucket: "book-app-654a8.appspot.com",
+  messagingSenderId: "114679969960",
+  appId: "1:114679969960:web:6de6311e0af023531caa9d",
+  measurementId: "G-33Q3YCJVQ9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
