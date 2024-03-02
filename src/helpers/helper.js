0900b830e97700ca2eaef78114813199a572f479
Ricardo Pimienta <pimientaglez@gmail.com>
2024-03-01 20:03:27 -0600

    add validation to authors

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -2,7 +2,7 @@ export const convertBook = (googleBook) => {
     return {
         id: googleBook.id,
         title: googleBook.volumeInfo.title,
-        author: googleBook.volumeInfo.authors[0],
+        author: googleBook.volumeInfo.authors? googleBook.volumeInfo.authors[0]: '',
         cover: googleBook.volumeInfo.imageLinks?  googleBook.volumeInfo.imageLinks.thumbnail : '',
         intro: googleBook.volumeInfo.description,
         completed: false,
